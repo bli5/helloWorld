9dfeb55ebffa01a767076c2e40ef56194e4fed61
Li Bin <thelastbin@gmail.com>
2018-07-31 20:20:45 +0000

    add dragment and dialog popup

---
+++ webApp/controller/HelloPanel.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+      onShowHello : function () {
+         // read msg from i18n model
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+         // show message
+         MessageToast.show(sMsg);
+      },
+      onOpenDialog : function () {
+      	var oView = this.getView();
+      	var oDialog = oView.byId("helloDialog");
+      	// crate dialog lazily
+      	if (!oDialog) {
+      		console.log(!oDialog);
+      		oDialog = sap.ui.xmlfragment(oView.getId(), "sap.ui.demo.walkthrough.view.HelloDialog");
+      		oView.addDependent(oDialog);
+      		
+      	}
+      	oDialog.open();
+      }
+   });
+});
